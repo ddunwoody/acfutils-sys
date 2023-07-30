063b9f84b197dcdea3d96adef27b722738019d16
David Dunwoody <david.dunwoody@gmail.com>
2023-07-30 12:49:29 +0100

    Convert example to test

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,23 @@
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
 include!("bindings.rs");
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn can_calculate_gc() {
+        let egll = crate::geo_pos2_t {
+            lat: 51.468,
+            lon: -0.4551,
+        };
+
+        let kjfk = crate::geo_pos2_t {
+            lat: 40.6398,
+            lon: -73.7789,
+        };
+        unsafe {
+            let distance = crate::gc_distance(egll, kjfk);
+            assert_eq!((distance / 1852.0).round(), 3000.0);
+        }
+    }
+}
